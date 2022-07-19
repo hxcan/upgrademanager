1d9a120e5d51678c25ae38d045aacebae4ec450f
Hxcan Cai <caihuosheng@gmail.com>
2022-07-19 19:32:59 +0800

    Fix problem, request permission of installing package

--- voiceui.inftp/src/main/java/com/stupidbeauty/upgrademanager/asynctask/LoadVoicePackageUrlMapInterface.java
+++ voiceui.inftp/src/main/java/com/stupidbeauty/upgrademanager/asynctask/LoadVoicePackageUrlMapInterface.java
@@ -1,20 +1,18 @@
 package com.stupidbeauty.upgrademanager.asynctask;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+// import android.content.Intent;
+// import android.content.IntentFilter;
 import android.database.Cursor;
-import android.net.Uri;
-import java.util.HashMap;
+// import android.net.Uri;
 import android.view.View;
-import android.os.AsyncTask;
+// import android.os.AsyncTask;
 import java.util.HashMap;
 
 public interface LoadVoicePackageUrlMapInterface
 {
   Context getContext(); //!< Get context.
-  
-  void setPackageNameUrlMap (HashMap<String, String> packageNameUrlMap) ;
+  void setPackageNameUrlMap (HashMap<String, String> packageNameUrlMap) ; //!< Set the pckage name url map.
 
   void setPackageNameVersionNameMap (HashMap<String, String> packageNameVersionNameMap); //!< 包名与可用版本号之间的映射关系。
-}
+} // public interface LoadVoicePackageUrlMapInterface
